c523f4afdc8bc10efd65353c71e21bfc6bfb4767
Jorge Chan <jorgeluis.chanchan@gmail.com>
2022-05-27 13:32:03 -0500

    Se eliminó las llaves del namespace de las clases y se agregó ejemplo de inyección de dependencias.

---
+++ webapinet/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using webapinet.Services;
+
+namespace webapinet.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HelloWorldController : ControllerBase
+{
+    IHelloWorldService helloWorldService;
+
+    public HelloWorldController(IHelloWorldService helloWorld)
+    {
+        helloWorldService = helloWorld;
+    }
+
+    public IActionResult Get()
+    {
+        return Ok(helloWorldService.GetHelloWorld());
+    }
+}
--- webapinet/Program.cs
+++ webapinet/Program.cs
@@ -1,4 +1,5 @@
 using webapinet.Middlewares;
+using webapinet.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// builder.Services.AddScoped<IHelloWorldService, HelloWorldService>(); // Dependency injection with an interface
+builder.Services.AddScoped<IHelloWorldService>(p => new HelloWorldService()); // Dependency injection with a class
 
 var app = builder.Build();
 
