ca85c3c448e431d3b929af25eca45a237bfca582
Jorge Chan <jorgeluis.chanchan@gmail.com>
2022-05-28 11:43:29 -0500

    Se configuró la inyección de dependencias de categoría y tareas.

--- webapinet/Program.cs
+++ webapinet/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 // builder.Services.AddScoped<IHelloWorldService, HelloWorldService>(); // Dependency injection with an interface
 builder.Services.AddScoped<IHelloWorldService>(p => new HelloWorldService()); // Dependency injection with a class
+builder.Services.AddScoped<ICategoriaService, CategoriaService>();
+builder.Services.AddScoped<ITareaService, TareaService>();
 
 var app = builder.Build();
 
